5e80218665c63cb96437ace23facfc179f24aa57
jfpratt@gmail.com <jfpratt@gmail.com@8c3eb09c-681d-5b01-5377-0e20a4091d4e>
2013-01-24 04:44:30 +0000

    Adding initial Unity project

---
+++ trunk/unity/Erganamics/Assets/Code/Util.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Erganamics {
+    public class Util {
+        public static GameObject CreateQuad() {
+            GameObject quad = new GameObject( "Quad", typeof( MeshFilter ), typeof( MeshRenderer ) );
+            Mesh mesh = new Mesh();
+
+            mesh.vertices = new Vector3[] {
+                new Vector3( 0f, 0f, 0f ),
+                new Vector3( 0f, 1f, 0f ),
+                new Vector3( 1f, 1f, 0f ),
+                new Vector3( 1f, 0f, 0f ),
+            };
+
+            mesh.uv = new Vector2[] {
+                new Vector2( 0f, 0f ),
+                new Vector2( 0f, 1f ),
+                new Vector2( 1f, 1f ),
+                new Vector2( 1f, 0f ),
+            };
+
+            mesh.triangles = new int[] {
+                0, 1, 2,
+                0, 2, 3,
+            };
+
+            quad.GetComponent<MeshFilter>().mesh = mesh;
+
+            return quad;
+        }
+    }
+}
