977f4d4af8b5a0c6a23cb482494ac4071c6c0275
shawnpatton@gmail.com <shawnpatton@gmail.com@8c3eb09c-681d-5b01-5377-0e20a4091d4e>
2013-01-26 05:56:22 +0000

    Touch activated color change random sound block awesomeness!

--- trunk/unity/Erganamics/Assets/Code/BackAndForth.cs
+++ trunk/unity/Erganamics/Assets/Code/BackAndForth.cs
@@ -12,6 +12,19 @@ public class BackAndForth : MonoBehaviour {
 	
 	// Update is called once per frame
 	private void Update () {
-        transform.position = new Vector3(amplitude*Mathf.Sin(Time.time), 0, 0);
+        //transform.position = new Vector3(amplitude*Mathf.Sin(Time.time), 0, 0);
+        foreach (Touch touch in Input.touches) {
+		    if (touch.phase == TouchPhase.Began) {
+		    	// Construct a ray from the current touch coordinates
+		    	var ray = Camera.main.ScreenPointToRay (touch.position);
+		    	if (Physics.Raycast (ray)) {
+		    		// Create a particle if hit
+                    transform.position = new Vector3(Random.Range(-10.0f, 10.0f), 0, 0);
+                    renderer.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+                    AudioSource[] sources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+                    sources[Random.Range(0, sources.Length-1)].Play();
+		    	}
+		    }
+	    }
 	}
 }
