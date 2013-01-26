f4bacc7eb0b8e488b4713c97bc393668c7d997ff
culyba@gmail.com <culyba@gmail.com@8c3eb09c-681d-5b01-5377-0e20a4091d4e>
2013-01-26 17:30:32 +0000

    Click and Drag added

---
+++ unity/Erganamics/Assets/Code/ETouch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Erganamics
+{
+    
+
+	class ETouch
+	{
+		public static int MOUSE_ID = -2;
+		
+        private Vector2 _deltaPosition;
+        private float _deltaTime;
+        private int _fingerId;
+        private TouchPhase _phase;
+        private Vector2 _position;
+        private Vector3 _position3;
+        private int _tapCount;
+
+        public ETouch(Vector2 position, TouchPhase phase) : this(position, phase, new Vector2(0,0))
+        {
+        }
+
+        public ETouch(Vector2 position, TouchPhase phase, Vector2 deltaPosition)
+            : this(position, phase, deltaPosition, -1, MOUSE_ID, 1)
+        {
+        }
+
+        public ETouch(Touch t)
+            : this(t.position, t.phase, t.deltaPosition, t.deltaTime, t.fingerId, t.tapCount)
+        {
+        }
+
+        public ETouch(Vector2 position, TouchPhase phase, Vector2 deltaPosition, float deltaTime, int fingerId, int tapCount)
+        {
+            this._deltaPosition = deltaPosition;
+            this._deltaTime = deltaTime;
+            this._phase = phase;
+            this._position = position;
+            this._fingerId = fingerId;
+            this._tapCount = tapCount;
+            this._position3 = new Vector3(this._position.x, this._position.y, 0);
+        }
+
+        public Vector2 deltaPosition { get { return this._deltaPosition; } }
+        public float deltaTime { get { return this._deltaTime; } }
+        public int fingerId { get { return this._fingerId; } }
+        public TouchPhase phase { get { return this._phase; } }
+        public Vector2 position { get { return this._position; } }
+        public Vector2 positionV3 { get { return this._position3; } }
+        public int tapCount { get { return this._tapCount; } } 
+	}
+}
---
+++ unity/Erganamics/Assets/Code/TouchUtils.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Erganamics {
+
+	class TouchUtils
+	{
+        private static ETouch mouseTouch = null;
+
+        public static ETouch[] GetTouches()
+        {
+            int touchIndex = 0;
+            if (Input.GetMouseButton(0))
+            {
+                touchIndex = 1;
+                if (mouseTouch == null)
+                {
+                    mouseTouch = new ETouch(new Vector2(Input.mousePosition.x, Input.mousePosition.y), TouchPhase.Began);
+                }
+                else
+                {
+                    Vector2 newPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                    Vector2 deltaPosition = new Vector2(newPos.x - mouseTouch.position.x, newPos.y - mouseTouch.position.y);
+                    TouchPhase phase;
+                    if (mouseTouch.deltaPosition.x == 0 && mouseTouch.deltaPosition.y == 0)
+                    {
+                        phase = TouchPhase.Stationary;
+                    }
+                    else
+                    {
+                        phase = TouchPhase.Moved;
+                    }
+                    mouseTouch = new ETouch(newPos, phase, deltaPosition);
+                }
+            }
+            else
+            {
+                if (mouseTouch != null)
+                {
+                    touchIndex = 1;
+                    Vector2 newPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                    Vector2 deltaPosition = new Vector2(newPos.x - mouseTouch.position.x, newPos.y - mouseTouch.position.y);
+                    mouseTouch = new ETouch(newPos, TouchPhase.Ended, deltaPosition);
+                }
+            }
+            ETouch[] touches = new ETouch[touchIndex + Input.touchCount];
+            if (touchIndex != 0)
+            {
+                touches[0] = mouseTouch;
+            }
+            foreach (Touch t in Input.touches) {
+                touches[touchIndex] = new ETouch(t);
+                touchIndex++;
+            }
+            if (mouseTouch != null && mouseTouch.phase == TouchPhase.Ended)
+            {
+                mouseTouch = null;
+            }
+            return touches;
+        }
+
+	}
+}
