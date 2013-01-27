5531399d7e3c30eba3e4cba649a1ea56bebea4dd
shawnpatton@gmail.com <shawnpatton@gmail.com@8c3eb09c-681d-5b01-5377-0e20a4091d4e>
2013-01-27 00:08:49 +0000

    States!! Enter and exit methods!

--- unity/Erganamics/Assets/Code/GameStateManager.cs
+++ unity/Erganamics/Assets/Code/GameStateManager.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using System.Collections;
+using System;
 
 public class GameStateManager : MonoBehaviour {
-
+    public Action exitCurrentState { get; set; }
     private Settings.GameState currentState = Settings.GameState.Title;
 
 	// Use this for initialization
 	void Start () {
-	    
+        changeStateTo(currentState);
 	}
 	
 	// Update is called once per frame
@@ -17,8 +18,71 @@ public class GameStateManager : MonoBehaviour {
 
     bool changeStateTo(Settings.GameState newState)
     {
-        Debug.Log("I'm supposed to change state to " + newState);
-        return false;
+        Debug.Log("I'm supposed to change state to '" + newState + "'");
+        if (exitCurrentState != null)
+        {
+            exitCurrentState();
+        }
+        if (newState == Settings.GameState.Title)
+        {
+            exitCurrentState = exitTitle;
+            return enterTitle();
+        }
+        else if (newState == Settings.GameState.MainGame)
+        {
+            exitCurrentState = exitMainGame;
+            return enterMainGame();
+        }
+        else if (newState == Settings.GameState.Win)
+        {
+            exitCurrentState = exitWin;
+            return enterWin();
+        }
+        else if (newState == Settings.GameState.Lose)
+        {
+            exitCurrentState = exitLose;
+            return enterLose();
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool enterTitle()
+    {
+        return true;
+    }
+
+    void exitTitle()
+    {
+    }
+
+    bool enterMainGame()
+    {
+        return true;
+    }
+
+    void exitMainGame()
+    {
+    }
+
+    bool enterWin()
+    {
+        return true;
+    }
+
+    void exitWin()
+    {
+    }
+
+    bool enterLose()
+    {
+        return true;
+    }
+
+    void exitLose()
+    {
     }
 
 
