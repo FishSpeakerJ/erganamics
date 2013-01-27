ea3cb35e34dc99e3df565c4d27f41bc0438b671c
culyba@gmail.com <culyba@gmail.com@8c3eb09c-681d-5b01-5377-0e20a4091d4e>
2013-01-27 05:44:45 +0000

    candy candyArea defined by quad

--- unity/Erganamics/Assets/Code/StuffManager.cs
+++ unity/Erganamics/Assets/Code/StuffManager.cs
@@ -6,9 +6,11 @@ using Erganamics;
 // This will create a pile of stuff (instances of PieceOfStuff) you can sift through
 public class StuffManager : MonoBehaviour {
 
+    public GameObject candyArea;
     public GameObject candyBag;
     public GameObject bagGlow;
     public GameStateManager gameStateManager;
+    public Material candyMaterial;
     public Rect bagRect;
 
     private List<PieceOfStuff> listOfStuff = new List<PieceOfStuff>();
@@ -20,12 +22,19 @@ public class StuffManager : MonoBehaviour {
         Bounds bagBounds = candyBag.collider.bounds;
         bagRect = new Rect(bagBounds.min.x, bagBounds.max.y, bagBounds.size.x, bagBounds.size.y);
 
+        float minX = candyArea.transform.position.x;
+        float maxX = minX + candyArea.collider.bounds.size.x;
+        float minY = candyArea.transform.position.y;
+        float maxY = minY + candyArea.collider.bounds.size.y;
+
+        Debug.Log(maxX + ", " + maxY);
+
         // Make the pieces of stuff
         for (int i = 0; i < Settings.Instance.piecesOfStuff; i++)
         {
             var quad = Util.CreateQuadAtRuntime();
             quad.name = string.Format("PieceOfStuff{0}", i);
-            quad.transform.position = new Vector3(Random.Range(Settings.Instance.stuffBoundsLow[0], Settings.Instance.stuffBoundsHigh[0]), Random.Range(Settings.Instance.stuffBoundsLow[1], Settings.Instance.stuffBoundsHigh[1]), Settings.Instance.stuffZ);
+            quad.transform.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Settings.Instance.stuffZ);
             listOfStuff.Add(quad.AddComponent<PieceOfStuff>());
 
         }
@@ -34,7 +43,9 @@ public class StuffManager : MonoBehaviour {
         {
             var quad = Util.CreateQuadAtRuntime();
             quad.name = string.Format("PieceOfCandy{0}", i);
-            quad.transform.position = new Vector3(Random.Range(Settings.Instance.stuffBoundsLow[0], Settings.Instance.stuffBoundsHigh[0]), Random.Range(Settings.Instance.stuffBoundsLow[1], Settings.Instance.stuffBoundsHigh[1]), Settings.Instance.candyZ);
+            quad.transform.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Settings.Instance.candyZ);
+            quad.renderer.material = candyMaterial;
+            quad.transform.Rotate(new Vector3(0,0, Random.Range(0f, 360f)));
             listOfCandy.Add(quad.AddComponent<PieceOfCandy>());
         }
 
